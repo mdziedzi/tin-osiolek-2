aae3377a7dfdd2a694f428fde78aa7a05cb2cff5
mdziedzi <marcindziedzic8@gmail.com>
2018-11-20 16:57:16 +0100

    Create menu with item.

--- app/src/main/java/com/marcindziedzic/osiolek2/connectToNet/ConnectToNetActivity.java
+++ app/src/main/java/com/marcindziedzic/osiolek2/connectToNet/ConnectToNetActivity.java
@@ -2,6 +2,9 @@ package com.marcindziedzic.osiolek2.connectToNet;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.marcindziedzic.osiolek2.R;
 
@@ -11,5 +14,18 @@ public class ConnectToNetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect_to_net);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // case jesli bedzie ich wiecej
+        return super.onOptionsItemSelected(item);
     }
 }
